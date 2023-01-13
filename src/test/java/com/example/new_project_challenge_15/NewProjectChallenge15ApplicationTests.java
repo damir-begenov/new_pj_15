50700d2d61bb77e08a0461d8f684469a7e729c6a
Damir <damir.ps04@gmail.com>
2023-01-14 03:23:01 +0600

    first commit

---
+++ src/test/java/com/example/new_project_challenge_15/NewProjectChallenge15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new_project_challenge_15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewProjectChallenge15ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
