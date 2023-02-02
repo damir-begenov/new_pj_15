ba7d34000de75ca6819522f3469df6afb9ba1e18
Damir <damir.ps04@gmail.com>
2023-02-03 02:45:32 +0600

    "sexyBack koshi"

---
+++ src/main/java/com/example/new_project_challenge_15/entity/rels/FOLLOWS.java
@@ -0,0 +1,39 @@
+package com.example.new_project_challenge_15.entity.rels;
+
+import com.example.new_project_challenge_15.entity.Movie;
+import com.example.new_project_challenge_15.entity.Person;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.TargetNode;
+
+public class FOLLOWS {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @TargetNode
+    private Person person;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public String toString() {
+        return "FOLLOWS{" +
+                "id=" + id +
+                ", person=" + person +
+                '}';
+    }
+}
--- src/main/java/com/example/new_project_challenge_15/service/PersonService.java
+++ src/main/java/com/example/new_project_challenge_15/service/PersonService.java
@@ -149,6 +149,7 @@ public class PersonService {
                     nodes.add(currMovie);
                 }
             }
+
         }
         doubleReturn doubleReturn = new doubleReturn(nodes, edges);
         return doubleReturn;
