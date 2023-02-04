924b47f6b41a773a2e0f2fa0068801c18d7a4bbc
Ma9ku <93972262+Ma9ku@users.noreply.github.com>
2023-02-04 20:37:31 +0600

    Front 04.02

--- frontend/src/Components/RightBar/RightBar.js
+++ frontend/src/Components/RightBar/RightBar.js
@@ -2,22 +2,20 @@ import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import './RightBar.css'
 
-class RightBar extends Component{
+class RightBar extends Component {
   render() {
     return (
       <div className='rightBar'>
         <div className="infoBlock" id="infoBlock">
           <div>
             <div className="infoBlockTitle">Информация о объекте</div>                
-            <div className="nodeInfo">
-              <div id="INFO1">NAME: <span id="nodeName"></span></div>
-              <div id="INFO2">Acted in: <span id="nodeActedIn"></span></div>
-              <div id="INFO3">Directed in: <span id="nodeDirectedIn"></span></div>
-              <div id="INFO4">Produced: <span id="nodeProduced"></span></div>
-              <div id="INFO5">Wrote: <span id="nodeWrote"></span></div>
-              <div id="INFO6">Reviewed: <span id="nodeReviewed"></span></div>
-            </div>
+            <div className="nodeInfo" id="nodeInfo"></div>
           </div>
+        </div>
+        <div className="actionBlock">
+          {true ? 
+            (<input type="button" visible="false" value="Show/Hide"/>) 
+            : ""}
         </div>
       </div>
     );
--- frontend/src/pages/Graphs/Graphs.js
+++ frontend/src/pages/Graphs/Graphs.js
@@ -19,26 +19,46 @@ import useFetch from "../../hooks/useFetch.js";
 var NoD =  [];
 var EdG = [];
 var Network;
+var SelectedNode = {}
+var SelectedEdge = {}
+
+var onSelectNode = false;
 
 export default class GraphNet extends Component {
     state = {
         nodes: [],
         edges: [],
         temp: '',
-        counter: 0,
-        selectNode: (event) => {
-          const selectedNode = Network.selectionHandler.selectionObj.nodes[Object.keys(Network.selectionHandler.selectionObj.nodes)[0]]
-          console.log(selectedNode)
+        counter: 0
+    }
 
-          this.createContextMenu(selectedNode.x, selectedNode.y)
+    assignInfoBlock = (options) => {
+      const infoBlock = document.querySelector(".nodeInfo")
+      Object.entries(options).forEach(entry => {
+        const [key, value] = entry;
+        const info = document.createElement("div")
+        info.innerHTML = `${key.toUpperCase()}: <span>${value}</span>`
 
-        },
-        oncontext: (event) => {
-        } 
+        infoBlock.appendChild(info)
+      });
     }
 
     createContextMenu = (x, y) => {
-      console.log(x, y)
+      const contextMenu = document.createElement("div")
+      contextMenu.classList.add("nodeContextMenu")
+      contextMenu.style.left = x+"px"
+      contextMenu.style.top = y+"px"
+
+      Network.focus(SelectedNode.id, {
+        scale: 0.6,
+        offset: {
+          x: 0,
+          y: 0
+        }
+      })
+
+      Network.canvas.body.container.style.position = "relative"
+      Network.canvas.body.container.appendChild(contextMenu)
     }
 
     numbers = {
@@ -80,19 +100,19 @@ export default class GraphNet extends Component {
     };
 
     movies = (url) => {
-
       axios.get(url).then(res => {
         let nodes = []
         const edges = res.data.edges;
 
+        edges.map(item => {
+          this.setEdgeSettings(item);
+        })
+
         let subNodes = []
         res.data.nodes.map(item => {
-          item.group = "selected"
-
+          this.setNodeSettings(item)
           nodes.push(item);
         })
-        // NoD = nodes
-        // EdG = edges 
 
         nodes.map(item => {
           item.label = item.name || item.roles[0]
@@ -105,37 +125,52 @@ export default class GraphNet extends Component {
       })
       
     }
-    handleSubmitDate = (options) => {
-      this.state.iin=""
-      this.state.iin2=""
-      axios.get("http://localhost:9090/connection/"+ options.date+"/"+options.date2+"/"+options.iin  )
-            .then(res => {
-                const nodes = res.data.nodes
-                const edges = res.data.edges
 
-                nodes.filter(e => e.main === true).map(item => (
-                  item.group = 'schools'
-                ))
-                nodes.filter(e => e.main === false && e.bin_IIN !== this.state.iin).map(item => (
-                  item.group = 'students'
-                ))
-                nodes.filter(e => e.bin_IIN === this.state.iin).map(item => (
-                  item.group = 'selected'
-                ))
+    setEdgeSettings = (edge) => {
+      edge.label = edge.type
+      Object.assign(edge, {properties: edge.properties})
 
-                nodes.map(item => {
-                  item.label = item.name
-                })
+      if (edge.type === 'acted_in') {
+        Object.assign(edge, {font: {color: this.colors.actedInEdge}})
+        Object.assign(edge, {color: this.colors.actedInEdge})
 
-                this.setState({nodes, edges})
-                this.state.isLoading = false
-                this.numbers.objects = nodes.length
-                this.numbers.relations = edges.length
-                NoD = nodes
-                EdG = edges
+      } else if (edge.type == 'directed') {
+        Object.assign(edge, {font: {color: this.colors.directedEdge}})
+        Object.assign(edge, {color: this.colors.directedEdge})
 
-                Network.fit({});
-            })
+      } else if (edge.type == 'reviewed') {
+        Object.assign(edge, {font: {color: this.colors.reviewEdge}})
+        Object.assign(edge, {color: this.colors.reviewEdge})
+
+      } else if (edge.type == 'wrote') {
+        Object.assign(edge, {font: {color: this.colors.wroteEdge}})
+        Object.assign(edge, {color: this.colors.wroteEdge})
+
+      } else if (edge.type == 'produced') {
+        Object.assign(edge, {font: {color: this.colors.producedEdge}})
+        Object.assign(edge, {color: this.colors.producedEdge})
+
+      } else if (edge.type == 'follows') {
+        Object.assign(edge, {font: {color: this.colors.followsEdge}})
+        Object.assign(edge, {color: this.colors.followsEdge})
+
+      }
+    }
+
+    setNodeSettings = (node) => {
+      if (node.description === "") {
+        // settings for actors
+        node.group = "actors"
+        node.mass = 5
+        
+
+      } else {
+        // settings for movies
+        node.group = "movies"
+        node.physics = false
+
+      }
+
     }
 
     setChange = (event) => {
@@ -143,44 +178,87 @@ export default class GraphNet extends Component {
     }
 
     colors = {
-      schoolColor: '#636951',
-      studentColor: '#393939',
-      studentColor2: '#393939',
-      selectedColor: '#2D4231', //Searched IIN
-      searchedColor: '#416748', //
+      actorIcon: '#cfcc53',
+      actorFont: '#cfcc53',
 
-      schoolFontColor: '#ffffff',
-      studentFontColor: '#ffffff',
-      studentFontColor2: '#ffffff',
-      selectedFontColor: '#DE191C',
-      searchedFontColor: '#99191C',
+      movieIcon: '#1c4709',
+      movieFont: '#1c4709',
 
-      edgeColor: '#888888'
+      wroteEdge: 'yellow',
+      reviewEdge: 'blue',
+      directedEdge: 'green',
+      actedInEdge: 'red',
+      producedEdge: 'pink',
+      followsEdge: 'purple'
     }
 
     options = {
       autoResize: true,
       edges: {
-        color: this.colors.edgeColor,
+        // color: 'white',
+        length: 100,
         width: 1,
-        arrows: "none",
+        selectionWidth: 5,
+        arrows: {
+          to: true,
+        },
+        smooth: {
+          "type": "dynamic",
+          "forceDirection": "none",
+          roundness: 0.5
+        }, 
         font: {
           strokeWidth: 0,
-          size: 10
-        }
+          size: 10,
+          align: "top"
+        },
+      },
+      physics: {
+        enabled: true,
+        barnesHut: {
+          springConstant: 0,
+          theta: 0.5,
+          gravitationalConstant: -2000,
+          centralGravity: 0.3,
+          springLength: 95,
+          springConstant: 0.04,
+          damping: 0.09,
+          avoidOverlap: 0
+        },
+        maxVelocity: 25,
+        minVelocity: 0,
+        solver: "barnesHut",
+        timestep: 0.5,
+        wind: { x: 0, y: 0 }
+
+        // forceAtlas2Based: {
+        //   gravitationalConstant: -26,
+        //   centralGravity: 0.005,
+        //   springLength: 230,
+        //   springConstant: 0.18,
+        //   avoidOverlap: 1.5
+        // },
+        // maxVelocity: 146,
+        // solver: 'forceAtlas2Based',
+        // timestep: 0.35,
+        // stabilization: {
+        //   enabled: true,
+        //   iterations: 1000,
+        //   updateInterval: 25
+        // }
       },
       groups: {
         actors: {
           shape: "icon",
           icon: {
             face: '"Font Awesome 5 Free"',
-            code: '\uf1ad',
+            code: '\uf007',
             weight: 700,
-            size: 70,
-            color: this.colors.schoolColor
+            size: 30,
+            color: this.colors.actorIcon
           },
           font: {
-            color: this.colors.schoolFontColor,
+            color: this.colors.actorFont,
             weight: 300,
             size: 20
           },
@@ -190,44 +268,16 @@ export default class GraphNet extends Component {
           shape: "icon",
           icon: {
             face: '"Font Awesome 5 Free"',
-            code: '\uf2bd',
+            code: '\uf03d',
             weight: 700,
             size: 50,
-            color: this.colors.studentColor
+            color: this.colors.movieIcon
           },
           font: {
-            color: this.colors.studentFontColor,
+            color: this.colors.movieFont,
             weight: 300,
           }
-        },
-        directors: {
-          shape: "icon",
-          icon: {
-            face: '"Font Awesome 5 Free"',
-            code: '\uf2bd',
-            weight: 700,
-            size: 50,
-            color: this.colors.studentColor2
-          },
-          font: {
-            color: this.colors.studentFontColor2,
-            weight: 300,
-          }
-        },
-        selected: {
-          shape: "icon",
-          icon: {
-            face: '"Font Awesome 5 Free"',
-            code: '\uf2bd',
-            weight: 700,
-            size: 50,
-            color: this.colors.selectedColor
-          },
-          font: {
-            color: this.colors.studentFontColor,
-            weight: 300,
-          }
-        },
+        }
       },
       nodes: {
         font: {
@@ -235,41 +285,83 @@ export default class GraphNet extends Component {
         },
       },
       height: "100%",
+
+      layout: {
+        randomSeed: 1,
+        improvedLayout: true,
+      }
     };
 
     manipulation = {
       deleteNode: true,
     }
 
-    search(value) {
-      console.log(value)
+    events = {
+      selectNode: (event) => {
+        SelectedNode = Network.selectionHandler.selectionObj.nodes[Object.keys(Network.selectionHandler.selectionObj.nodes)[0]]
+        onSelectNode = true
 
-      const searchNodes = NoD.filter(elem => {
-        if (elem.name.toLowerCase().includes(value.toLowerCase())) {
-          return elem;
+        const infoBlock = document.querySelector(".nodeInfo")
+        infoBlock.innerHTML = ""
+
+        this.assignInfoBlock({
+          name: SelectedNode.options.name, 
+          year: SelectedNode.options.year
+        })
+
+        if (SelectedNode.options.group == "movies") {
+          this.assignInfoBlock({description: SelectedNode.options.description})
+        }
+      }, 
+      deselectNode: (event) => {
+        onSelectNode = false
+      },
+      selectEdge: (event) => {
+        SelectedEdge = Network.selectionHandler.selectionObj.edges[Object.keys(Network.selectionHandler.selectionObj.edges)[0]]
+
+        if (onSelectNode == true) return
+
+        const infoBlock = document.querySelector(".nodeInfo")
+        infoBlock.innerHTML = ""
+
+        const fromNode = Network.body.nodes[SelectedEdge.options.from]
+        const toNode = Network.body.nodes[SelectedEdge.options.to]
+
+        let edgeType = SelectedEdge.options.label.replace('_', ' ');
+        edgeType = edgeType.charAt(0).toUpperCase() + edgeType.slice(1)
+
+        let edgeProperties = [];
+        Object.entries(this.state.edges).forEach(item => {
+          if (item[1].id == SelectedEdge.id) {
+            edgeProperties = item[1].properties;
+          }
+        });
+
+        let edgeRoles = "none"
+        Object.entries(edgeProperties).forEach(item => {
+          console.log(item)
+          if (edgeRoles == "none") edgeRoles = item[1].value
+          else edgeRoles += ", " + item[1].value
+        })
+
+        this.assignInfoBlock({
+          from: fromNode.options.name,
+          to: toNode.options.name,
+          type: edgeType,
+          roles: edgeRoles
+        })
+      },
+    }
+
+    search(value) {
+      const searchNodes = Object.values(Network.body.nodes).filter(elem => {
+        if (elem.options.label != undefined && elem.options.label.toLowerCase().includes(value.toLowerCase())) {
+          // console.log(elem.options.label)
+          return true;
         }
       });
 
       const item = searchNodes[0];
-      // document.getElementById("nodeIin").innerHTML = item.bin_IIN;
-      // document.getElementById("nodeName").innerHTML = item.name;
-      // document.getElementById("nodeLabel").innerHTML = item.labl;
-
-      // for (const [key, value] of Object.entries(Network.body.nodes)) {
-      //   if (value.options.group != 'selected') {
-      //     value.options.icon.color = this.colors.studentColor;
-      //   }
-
-      //   if (value.options.group == 'schools') {
-      //     value.options.icon.color = this.colors.schoolColor;
-      //   } else if (value.options.group == 'selected') {
-      //     value.options.icon.color = this.colors.selectedColor;
-      //   }
-
-      //   if (value.options.id == item.id) {
-      //     value.options.icon.color = this.colors.searchedColor;
-      //   }
-      // }
 
       Network.focus(item.id, {
         scale: 2.5,
@@ -277,13 +369,6 @@ export default class GraphNet extends Component {
           x: 0,
           y: 0
         },
-        // animation: {
-        //   duration: 1,
-        //   easingFunction: 'easeInQuad'
-        // }
-      })
-
-      searchNodes.map(elem => {
       })
     }
 
@@ -293,13 +378,13 @@ export default class GraphNet extends Component {
           <div className='mainSection'>
           <>
             <LeftBar iin={this.state.iin} iin2={this.state.iin2} handleSubmit={this.Submit} setIIN={this.setChange}></LeftBar>
-              <div className='centralBar'>
-                <div className="waiterBox">
-                  {/* <a>Make a search</a> */}
-                  <i id="waiter" class="fa-solid fa-magnifying-glass"></i>
-                </div>
+            <div className='centralBar'>
+              <div className="waiterBox">
+                {/* <a>Make a search</a> */}
+                <i id="waiter" class="fa-solid fa-magnifying-glass"></i>
               </div>
-            <RightBar objects={this.numbers.objects} relations={this.numbers.relations} current={this.current}></RightBar>
+            </div>
+            <RightBar></RightBar>
           </>
           </div>
         )
@@ -313,7 +398,7 @@ export default class GraphNet extends Component {
                   <a>No objects found</a>
                 </div>
               </div>
-            <RightBar objects={this.numbers.objects} relations={this.numbers.relations} current={this.current}></RightBar>
+            <RightBar></RightBar>
           </>
           </div>
         )
@@ -329,7 +414,7 @@ export default class GraphNet extends Component {
                   <div class="inner three"></div>
                 </div>
               </div>
-            <RightBar objects={this.numbers.objects} relations={this.numbers.relations} current={this.current}></RightBar>
+            <RightBar></RightBar>
           </>
           </div>
         )
@@ -338,7 +423,7 @@ export default class GraphNet extends Component {
         <div className='mainSection'>
         <>
         <LeftBar iin={this.state.iin} iin2={this.state.iin2} handleSubmit={this.Submit} setIIN={this.setChange}></LeftBar>
-        <div className='centralBar'>
+        <div className='centralBar' id="centralBar">
             <div className="nodeSearch">
               <input type="text" id="nodeSearchInput" placeholder="Еще один поиск.." 
                 onKeyDown={event => {
@@ -361,14 +446,16 @@ export default class GraphNet extends Component {
             <Graph
               graph={this.state}
               options={this.options}
-              events={this.state}
+              events={this.events}
               getNetwork={network => {
                 Network = network;
               }}
               manipulation={this.manipulation}
+              className={"graph"}
             />
-          </div>
-        <RightBar objects={this.numbers.objects} relations={this.numbers.relations} current={this.current}></RightBar>
+        </div>
+          
+        <RightBar></RightBar>
         </>
         </div>
       )
