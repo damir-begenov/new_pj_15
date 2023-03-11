c246e8dbdef58aab828648b72abc0384111d1eb1
Ma9ku <93972262+Ma9ku@users.noreply.github.com>
2023-03-11 10:50:13 +0600

    admin page route validation

--- frontend/src/pages/AdminPage/AdminPage.js
+++ frontend/src/pages/AdminPage/AdminPage.js
@@ -1,53 +1,54 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import axios from 'axios';
 
 import './AdminPage.css'
 import UsersTable from "../../Components/UsersTable/UsersTable";
+import { useLocation, useNavigate } from "react-router-dom";
 
-class AdminPage extends Component {
-    constructor(props) {
-        super(props);
+const AdminPage = (props) => {
+    const userSession = JSON.parse(localStorage.getItem("user"))
+    const navigate = useNavigate()    
 
-        this.state = {
-        }
-    }
-    state = {
-        users: 0,
-        logs: 0
-    }
+    const [users, setUsers] = useState(0)
+    const [logs, setLogs] = useState(0)
 
-    componentDidMount() {
+    const location = useLocation()
+
+    useEffect(() => {
+        const a = !userSession.roles.includes("ADMIN") ? navigate('/') : ""
+
         axios.get(`http://localhost:9091/api/finpol/main/general`)
         .then(res => {
-            this.setState({users: res.data.allRequsetNum, logs: res.data.todayRequsetNum})
+            setUsers(res.data.allRequsetNum)
+            setLogs(res.data.todayRequsetNum)
+
+            console.log(this.location)
+        })
     })
-    }
 
-    render() {
-        return (
-            <section>
-                <div className="countStats">
-                    <div className="lastQuery">
-                        <div>Количество посещений</div>
-                        <div>1123</div>
-                    </div>
-
-                    <div>
-                        <div>Количество пользователей</div>
-                        <div>{this.state.users}</div>
-                    </div>
-
-                    <div>
-                        <div>Количество запросов</div>
-                        <div>{this.state.logs}</div>
-                    </div>
+    return (
+        <section>
+            <div className="countStats">
+                <div className="lastQuery">
+                    <div>Количество посещений</div>
+                    <div>1123</div>
                 </div>
+
                 <div>
-                    <UsersTable></UsersTable>
+                    <div>Количество пользователей</div>
+                    <div>{users}</div>
                 </div>
-          </section>
-        );
-    }
+
+                <div>
+                    <div>Количество запросов</div>
+                    <div>{logs}</div>
+                </div>
+            </div>
+            <div>
+                <UsersTable></UsersTable>
+            </div>
+        </section>
+    );
 }
 
 export default AdminPage;
